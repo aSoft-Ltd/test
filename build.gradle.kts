30bd33c3158545e1cef925c74884d56ed2f4a7c9
andylamax <andylamax@programmer.net>
2020-11-12 10:32:02 +0300

    Minimzed intensity of build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     id("com.android.library") version "4.1.0"
     kotlin("multiplatform") version "1.4.10"
-    id("tz.co.asoft.library")
-    id("io.codearte.nexus-staging") version ("0.22.0")
+    id("tz.co.asoft.library") version "0.0.6"
+    id("io.codearte.nexus-staging") version "0.22.0"
     signing
 }
 
 object vers {
     val selenium = "4.0.0-alpha-6"
+    val asoft_test = "1.0.1"
 
     object androidx {
         val espresso = "3.1.0"
@@ -15,17 +16,6 @@ object vers {
         val test_rules = "1.1.0"
     }
 }
-
-group = "tz.co.asoft"
-version = "1.0.1"
-description = "A Multiplatform testing library"
-url = "https://github.com/aSoft-Ltd/test"
-scmConnection = "scm:git:git://github.com/aSoft-Ltd/test.git"
-scmDeveloperConnection = "scm:git:https://github.com/aSoft-Ltd/test.git"
-licenseName = "MIT License"
-licenseUrl = "https://github.com/aSoft-Ltd/test/blob/master/LICENSE"
-developerId = "andylamax"
-developerName = "Anderson Lameck"
 
 kotlin {
     universalLib()
@@ -64,20 +54,7 @@ kotlin {
     }
 }
 
-val nexusUsername = System.getenv("ASOFT_NEXUS_USERNAME") ?: "null" //"P0ZW8VYf"
-val nexusPassword = System.getenv("ASOFT_NEXUS_PASSWORD") ?: "null" //"bhbdCSvPlI0Y9OidU8uLxx205/XWJTEMeCQhKE+T3I1B"
-val pgpPrivateKey = System.getenv("ASOFT_MAVEN_PGP_PRIVATE_KEY") ?: ""
-val pgpPassword = System.getenv("ASOFT_MAVEN_PGP_PASSWORD") ?: ""
-
-nexusStaging {
-    username = nexusUsername
-    password = nexusPassword
-}
-
-configureSigning(privateKey = pgpPrivateKey, password = pgpPassword)
-
-configurePublishing {
-    repositories {
-        configureMavenCentralRepos(username = nexusUsername, password = nexusPassword)
-    }
-}
+aSoftPublicLibrary(
+    version = vers.asoft_test,
+    description = "A Multiplatform testing library"
+)

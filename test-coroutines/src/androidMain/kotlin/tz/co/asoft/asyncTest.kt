4c058f4f9c4db51d6529b1c84332c1c18f86a64e
andylamax <andylamax@programmer.net>
2020-12-29 08:44:24 +0300

    Fixed some minor bug

--- test-coroutines/src/androidMain/kotlin/tz/co/asoft/asyncTest.kt
+++ test-coroutines/src/androidMain/kotlin/tz/co/asoft/asyncTest.kt
@@ -3,6 +3,4 @@ package tz.co.asoft
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
-actual fun asyncTest(block: suspend CoroutineScope.() -> Any?): Unit = runBlocking {
-    block()
-}
+actual fun asyncTest(block: suspend CoroutineScope.() -> Any?): Unit = runBlocking { block() }
--- test-coroutines/src/jvmMain/kotlin/tz/co/asoft/asyncTest.kt
+++ test-coroutines/src/jvmMain/kotlin/tz/co/asoft/asyncTest.kt
@@ -3,4 +3,4 @@ package tz.co.asoft
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
-actual fun asyncTest(block: suspend CoroutineScope.() -> Unit) = runBlocking(block = block)
+actual fun asyncTest(block: suspend CoroutineScope.() -> Any?): Unit = runBlocking { block() }

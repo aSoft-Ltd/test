c7bde615014de8eea92af42d6851650ad5407f23
andylamax <andylamax@programmer.net>
2021-06-05 03:01:19 +0300

    Updated readme

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -3,18 +3,12 @@ object vers {
     val nexus_staging = "0.22.0"
     val selenium = "4.0.0-alpha-6"
 
-    object asoft {
-        val builders = "1.3.30"
-        val test = "1.1.31"
-    }
-
-    object androidx {
-        val espresso = "3.1.0"
-        val test_runner = "1.1.0"
-        val test_rules = "1.1.0"
-    }
-
     object kotlinx {
         val coroutines = "1.5.0"
+    }
+
+    object asoft {
+        val test = "1.1.32"
+        val builders = "1.3.30"
     }
 }
